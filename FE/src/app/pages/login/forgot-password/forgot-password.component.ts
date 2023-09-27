4a1341a90d2a25d30efc5c26f60b9d5afaddfe35
Comehanoi <111983108+Comehanoi@users.noreply.github.com>
2023-09-27 16:25:46 +0700

    fix template forgotPassword and add notification

--- FE/src/app/pages/login/forgot-password/forgot-password.component.ts
+++ FE/src/app/pages/login/forgot-password/forgot-password.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from 'express';
+import { Router } from '@angular/router';
 import { LoginService } from '../services/login.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
@@ -11,7 +11,8 @@ export class ForgotPasswordComponent implements OnInit {
   email: any
 
   constructor(
-
+    public loginServices: LoginService,
+    private router: Router
   ) {
 
   }
@@ -20,6 +21,42 @@ export class ForgotPasswordComponent implements OnInit {
 
   }
   sendMail() {
-
+    if(this.email){
+      this.router.navigate(['/login']);
+      this.processResponse(true)
+    }
+    else{
+      this.processResponse(false)
+    }
+  }
+  backToLogin(){
+    this.router.navigate(['/login']);
+  }
+  backToRegister(){
+    this.router.navigate(['/login/register']);
+  }
+  processResponse(res: any) {
+    if (res == true) {
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        title: 'Gửi Mã Thành Công',
+        icon: 'success',
+      });
+    }
+    if (res == false) {
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        title: 'Gửi Mã Thất Bại',
+        icon: 'error',
+      });
+    }
   }
 }
