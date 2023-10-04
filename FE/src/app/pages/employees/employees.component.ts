22a538c73a1e1172986eb3746c5ff8a1acbec0f6
Comehanoi <111983108+Comehanoi@users.noreply.github.com>
2023-10-04 10:26:55 +0700

    fix template add search for employees

--- FE/src/app/pages/employees/employees.component.ts
+++ FE/src/app/pages/employees/employees.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteBookRoomComponent } from '../bookRooms/delete-book-room/delete-book-room.component';
+import { PutBookRoomComponent } from '../bookRooms/put-book-room/put-book-room.component';
 
 @Component({
   selector: 'app-employees',
@@ -6,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-
+  searchParam:any
+  selected :any;
+  searchByPlaceParam: any
   constructor(
-
+    public dialog: MatDialog,
   ) {
 
    }
@@ -17,4 +22,45 @@ export class EmployeesComponent implements OnInit {
     debugger
   }
 
+  search(searchParam){
+
+  }
+
+  fetch(){
+
+  }
+
+  fixBookRoom(){
+    let dialogRef = this.dialog.open(PutBookRoomComponent, {
+      panelClass: "add-processcode-dialog",
+      data: {
+          title: 'Thông tin đặt phòng',
+      },
+  });
+
+  dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+          this.fetch();
+      }
+  });
+  }
+
+  deleteBookRoom(){
+    let dialogRef = this.dialog.open(DeleteBookRoomComponent, {
+      panelClass: "add-processcode-dialog",
+      data: {
+          title: 'Thông tin đặt phòng',
+      },
+  });
+
+  dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+          this.fetch();
+      }
+  });
+  }
+
+  searchByPlace(searchByPlaceParam) {
+
+  }
 }
--- FE/src/app/pages/hotels/hotels.component.ts
+++ FE/src/app/pages/hotels/hotels.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HotelsService } from '../services/hotels.service';
+import Swal from 'sweetalert2';
+import { BookNewRoomsComponent } from '../bookRooms/book-new-rooms/book-new-rooms.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-hotels',
@@ -9,7 +12,9 @@ import { HotelsService } from '../services/hotels.service';
 export class HotelsComponent implements OnInit {
 
   constructor(
-    public hotelsServices: HotelsService
+    public hotelsServices: HotelsService,
+    public dialog: MatDialog,
+
   ) {
 
   }
@@ -20,6 +25,47 @@ export class HotelsComponent implements OnInit {
 
   fetch(){
 
+  }
+  bookRoomNow(){
+    let dialogRef = this.dialog.open(BookNewRoomsComponent, {
+      panelClass: "add-processcode-dialog",
+      data: {
+        title: 'Thông tin đặt phòng',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.fetch();
+        this.processResponse(result)
+      }
+    });
+  }
+
+  processResponse(res:any) {
+    if(res == 'Thêm mới thành công'){
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        title: 'Thêm mới Thành Công',
+        icon: 'success',
+      });
+    }
+
+    else{
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        title: 'Thêm mới Thất Bại',
+        icon: 'error',
+      });
+    }
   }
 
 }
